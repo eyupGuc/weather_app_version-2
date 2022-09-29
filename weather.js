59739ed16b500d21c6dac6ce117ab7fa8fc5243e
eyupGuc <meydanguresi@gmail.com>
2022-09-29 19:56:31 +0300

    move

--- weather.js
+++ weather.js
@@ -23,11 +23,13 @@ form.addEventListener("submit", (e) => {
 });
 
 //Get api function
-const getWeatherDataFromApi = () => {
+const getWeatherDataFromApi = async() => {
   // alert("http request is gone");
   const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"));
   alert(tokenKey);
   console.log(tokenKey);
   const inputValue=input.value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${}&appid=e815a95d20a585101c219591fd494992&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
+
+  const response=await fetch(url).then()
 };
