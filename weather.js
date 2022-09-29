546018d661e27e83b783722bee62c8b0f0217f41
eyupGuc <meydanguresi@gmail.com>
2022-09-29 08:21:30 +0300

    locale storage

--- weather.js
+++ weather.js
@@ -1,2 +1,11 @@
-const form = document.querySelector(".top-banner form");
-console.log(form);
+const form = document.querySelector("section.top-banner form");
+// console.log(form);
+const input = document.querySelector(".container input");
+// console.log(input);
+
+const msg = document.querySelector("span.msg");
+const list = document.querySelector(".ajax-section .cities");
+console.log(list);
+/*const list=document.querySelector(".ajax-section.cities"); eğer bu şekilde .cities ile .ajax-section bitişik olursa tek ikisinde de ortak bir class var  */
+
+localStorage.setItem("tokenKey", "e815a95d20a585101c219591fd494992");
